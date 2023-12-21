cac7c1bf3cf6df6b4866215c60a60c4a5a94d5fe
33arsenicum <cmykbielaruski@gmail.com>
2023-12-21 22:21:02 +0300

    feat: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this.crypt(message, key, true);
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this.crypt(encryptedMessage, key, false);
+  }
+
+  crypt(message, key, encrypt) {
+    let result = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        let shift = this.alphabet.indexOf(key[j % key.length]);
+        if (!encrypt) {
+          shift = 26 - shift;
+        }
+        result += this.alphabet[(this.alphabet.indexOf(message[i]) + shift) % 26];
+        j++;
+      } else {
+        result += message[i];
+      }
+    }
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
